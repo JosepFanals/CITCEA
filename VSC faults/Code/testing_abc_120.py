6529835720ba8c003513c3caa65f2bcbfb37de8c
unknown <jfanals13@gmail.com>
2021-02-21 20:00:49 +0100

    LLG done

--- VSC faults/Code/testing_abc_120.py
+++ VSC faults/Code/testing_abc_120.py
@@ -94,3 +94,51 @@ print(Vc0, Vc1, Vc2)
 Vcc012 = np.array([Vc0, Vc1, Vc2])
 Vccabc = V012_to_abc(Vcc012)
 print(Vccabc)
+
+
+print('=========')
+
+# LLG FAULT
+
+Va = Ia * (Z1 + Z2) + Vga
+# Vb = Ib * Z1 + 1 / (3 * Z2 + Zf) * (Ic * (Z2 * Z2) + Ib * (Z2 * Zf + Z2 * Z2) + Vgc * Z2 + Vgb * (Z2 + Zf))
+# Vc = Ic * Z1 + (Zf + Z2) / Z2 * 1 / (3 * Z2 + Zf) * (Ic * (Z2 * Z2) + Ib * (Z2 * Zf + Z2 * Z2) + Vgc * Z2 + Vgb * (Z2 * Zf)) - Vgb * Zf / Z2 - Ib * Z2 * Zf / Z2
+# Vb = Vgb + Ia * (-2 * Z2) + Ic * Z2 + Z2 / (2 * Z2 + Zf) * (Vgc - Vgb - Zf * Ic + Z2 * Ia)
+# Vc = Ic * (2 * Z1 + Zf) + Ia * Z1 + Vgb + Ia * (-2 * Z2) + Ic * Z2 + Z2 / (2 * Z2 + Zf) * (Vgc - Vgb - Zf * Ic + Z2 * Ia) + Zf / (2 * Z2 + Zf) * (Vgc - Vgb - Zf * Ic + Z2 * Ia)
+
+# Vy = 1 / (Zf + Z2 - Z2 * Z2 / (Zf + Z2)) * (Z2 * Zf * Ic + Zf * Vgc + Z2 / (Zf + Z2) * (Z2 * Zf * Ib + Zf * Vgb))
+# Vy = (Zf + Z2) / (Zf * Zf + 2 * Zf * Z2) * (Z2 * Zf * Ic + Zf * Vgc + Z2 / (Zf + Z2) * (Z2 * Zf * Ib + Zf * Vgb))
+Vy = 1 / (Zf + 2 * Z2) * (Ic * (Z2 * (Zf + Z2)) + Vgc * (Zf + Z2) + Ib * Z2 * Z2 + Vgb * Z2)
+#Vx = 1 / (Zf + Z2) * (Z2 * Zf * Ib + Zf * Vgb + Z2 * Vy)
+Vx = 1 / ((Zf + Z2) * (Zf + 2 * Z2)) * (Ib * (Z2 * Zf * Zf + 2 * Z2 * Z2 * Zf + Z2 * Z2 * Z2) + Ic * (Z2 * Z2 * Zf + Z2 * Z2 * Z2) + Vgb * (Zf * Zf + 2 * Zf * Z2 + Z2 * Z2) + Vgc * (Z2 * Zf + Z2 * Z2))
+
+# Vy = 1 / (Zf + 2 * Z2) * (Ic * (Z2 * Zf + Z2 * Z2) + Ib * (Z2 * Zf) + Vgc * (Zf + Z2) + Vgb * Z2)
+# Vx = 1 / (Zf * Zf + 3 * Z2 * Zf + 2 * Z2 * Z2) * (Ib * (Z2 * Zf * Zf + 3 * Z2 * Z2 * Zf) + Vgb * (Zf * Zf + 2 * Zf * Z2 + Z2 * Z2) + Ic * (Z2 * Z2 * Zf + Z2 * Z2 * Z2) + Vgc * (Z2 * Zf + Z2 * Z2))
+
+Vc = Vy + Ic * Z1
+Vb = Vx + Ib * Z1
+
+# print(Vy, Vx)
+
+print(Va, Vb, Vc)
+
+
+# Vc2 = 1 / (2 * Z2 + Zf) * (1 + I1 * (Z1 + Z2) + I2 * (Z1 + Z2) * Zf / Z2)
+# Vc1 = 1 + I1 * (Z1 + Z2) + I2 * (Z1 + Z2) - Vc2
+# Vc1 = 1 / (Zf * Zf + 3 * Z2 * Zf + 2 * Z2 * Z2) * (1 * (Zf * Zf + Z2 * Z2 + 2 * Z2 * Zf) + I1 * (2 * Z1 * Z2 * Z2 + 3 * Z1 * Z2 * Zf + 2 * Z2 * Z2 * Zf + 2 * Z1 * Z2 * Z2 + Z2 * Z2 * Z2) + I2 * (Z2 * Z2 * Zf + Z2 * Z2 * Z2))
+# Vc2 = 1 / (Zf + 2 * Z2 * Zf) * (1 * Z2 * Zf + I1 * (Z2 * Z2 * Zf) + I2 * (Z1 * Zf * Zf + Z2 * Zf * Zf + 2 * Z1 * Z2 * Zf + Z2 * Z2 * Zf))
+
+#Ix = 1 / (2 * Z2 + Zf) * (1 + I1 * Z2 - I2 * Z2)
+#Vc2 = 1 + I1 * Z2 + I2 * Z1 - Ix * (Z2 + Zf)
+#Vc1 = Z1 * I1 + Z2 * (I1 - Ix) + 1
+
+Vc1 = 1 + I1 * Z1 + I1 * Z2 - Z2 / (2 * Z2 + Zf) * (1 + I1 * Z2 - I2 * Z2)
+Vc2 = 1 + I1 * Z2 + I2 * Z1 - (Z2 + Zf) / (2 * Z2 + Zf) * (1 + I1 * Z2 - I2 * Z2)
+
+Vc0 = 0
+Vccc012 = np.array([Vc0, Vc1, Vc2])
+Vcccabc = V012_to_abc(Vccc012)
+print(Vcccabc)
+
+
+
