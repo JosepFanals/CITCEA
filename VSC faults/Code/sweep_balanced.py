e3c5e748fbc70db922d389cf93008ed6925039f2
unknown <jfanals13@gmail.com>
2021-02-23 10:20:40 +0100

    plotting triangle

--- VSC faults/Code/sweep_balanced.py
+++ VSC faults/Code/sweep_balanced.py
@@ -39,8 +39,20 @@ def V012_to_abc(V012):
     return Vabc
 
 
+def fVabc(Iabc):  # this is for the balanced
+    Ia = Iabc[0]
+    Ib = Iabc[1]
+    Ic = Iabc[2]
+    
+    Va = 1 / (Zf + Z2) * (Ia * (Z1 * Z2 + Z2 * Zf + Zf * Z1) + Vga * Zf)
+    Vb = 1 / (Zf + Z2) * (Ib * (Z1 * Z2 + Z2 * Zf + Zf * Z1) + Vgb * Zf)
+    Vc = 1 / (Zf + Z2) * (Ic * (Z1 * Z1 + Z2 * Zf + Zf * Z1) + Vgc * Zf)
+    Vabc = np.array([Va, Vb, Vc])
+    return Vabc
+
+
 # initialization
-Zf = 0.2 * 1j
+Zf = 0.05 * 1j
 Z2 = 0.01 + 0.05 * 1j
 Z1 = 0.02 + 0.1 * 1j
 Imax = 1
@@ -56,7 +68,7 @@ Ib_re = 0
 Ib_im = 0
 Ic_re = 0
 Ic_im = 0
-n_iter = 10
+n_iter = 40
 compt = 0
 
 Ia_re_vec = []
@@ -103,10 +115,8 @@ for kk in range(n_iter):
                     Ib = Ib_re + 1j * Ib_im
                     Ic = Ic_re + 1j * Ic_im
                     Iabc = np.array([Ia, Ib, Ic])
-                    Va = 1 / (Zf + Z2) * (Ia * (Z1 * Z2 + Z2 * Zf + Zf * Z1) + Vga * Zf)
-                    Vb = 1 / (Zf + Z2) * (Ib * (Z1 * Z2 + Z2 * Zf + Zf * Z1) + Vgb * Zf)
-                    Vc = 1 / (Zf + Z2) * (Ic * (Z1 * Z1 + Z2 * Zf + Zf * Z1) + Vgc * Zf)
-                    Vabc = np.array([Va, Vb, Vc])
+
+                    Vabc = fVabc(Iabc)
                     V012 = Vabc_to_012(Vabc)
 
                     V0_re_vec.append(np.real(V012[0]))
@@ -137,10 +147,27 @@ for kk in range(n_iter):
 
 V1_abs_vec = []
 V2_abs_vec = []
+V12_abs_vec = []  # maximum difference between abs(V1) and abs(V2)
+V12_max = 0
+ind_max = 0
 
 for kk in range(compt):
     V1_abs_vec.append(np.sqrt(V1_re_vec[kk] ** 2 + V1_im_vec[kk] ** 2))
     V2_abs_vec.append(np.sqrt(V2_re_vec[kk] ** 2 + V2_im_vec[kk] ** 2))
+    V12_diff = V1_abs_vec[kk] - V2_abs_vec[kk]
+    V12_abs_vec.append(V12_diff)
+    V12_max = max(V12_diff, V12_max)
+    if V12_max == V12_diff:
+        ind_max = kk
+
+print('Objective function: ', V12_max)
+Ia_ff = Ia_re_vec[ind_max] + 1j * Ia_im_vec[ind_max]
+Ib_ff = Ib_re_vec[ind_max] + 1j * Ib_im_vec[ind_max]
+Ic_ff = Ic_re_vec[ind_max] + 1j * Ic_im_vec[ind_max]
+print('abc currents: ', Ia_ff, Ib_ff, Ic_ff)
+
+
 
 plt.scatter(V1_abs_vec, V2_abs_vec, s=1)
 plt.show()
+
