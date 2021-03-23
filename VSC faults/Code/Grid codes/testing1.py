77ac7d6a6a6464cd39340b82de981973425555d6
unknown <jfanals13@gmail.com>
2021-03-23 10:52:02 +0100

    grid code first example done

--- VSC faults/Code/Grid codes/testing1.py
+++ VSC faults/Code/Grid codes/testing1.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-Zf = 0.08 + 0.00 * 1j  # fault impedance
+Zf = 0.03 + 0.00 * 1j  # fault impedance
 Z1 = 0.01 + 0.10 * 1j  # Za in the drawings
 Z2 = 0.01 + 0.05 * 1j  # Zth in the drawings
 Imax = 1
@@ -50,24 +50,24 @@ def V012_to_abc(V012):
 
 def V0(x):
     # V0 = 0  # balanced
-    # V0 = - Z2 / (3 * Zf + 3 * Z2) * (Vth_1 + (x[0] + 1j * x[1]) * Z2 + (x[2] + 1j * x[3]) * Z2)  # LG
-    V0 = 0  # LL
+    V0 = - Z2 / (3 * Zf + 3 * Z2) * (Vth_1 + (x[0] + 1j * x[1]) * Z2 + (x[2] + 1j * x[3]) * Z2)  # LG
+    # V0 = 0  # LL
     # V0 = Z2 / (3 * Z2 + 6 * Zf) * ((x[0] + 1j * x[1]) * Z2 + (x[2] + 1j * x[3]) * Z2 + Vth_1)  # LLG
     
     return V0
 
 def V1(x):
     # V1 = 1 / (Zf + Z2) * (Vth_1 * Zf + (x[0] + 1j * x[1]) * (Z1 * Zf + Z1 * Z2 + Zf * Z2))  # balanced
-    # V1 = (x[0] + 1j * x[1]) * (Z1 + Z2) + Vth_1 - Z2 / (3 * Zf + 3 * Z2) * ((x[2] + 1j * x[3]) * Z2 + (x[0] + 1j * x[1]) * Z2 + Vth_1)  # LG
-    V1 = Vth_1 + (x[0] + 1j * x[1]) * Z1 + (x[0] + 1j * x[1]) * Z2 - Z2 / (2 * Z2 + Zf) * (Vth_1 + (x[0] + 1j * x[1]) * Z2 - (x[2] + 1j * x[3]) * Z2)  # LL
+    V1 = (x[0] + 1j * x[1]) * (Z1 + Z2) + Vth_1 - Z2 / (3 * Zf + 3 * Z2) * ((x[2] + 1j * x[3]) * Z2 + (x[0] + 1j * x[1]) * Z2 + Vth_1)  # LG
+    # V1 = Vth_1 + (x[0] + 1j * x[1]) * Z1 + (x[0] + 1j * x[1]) * Z2 - Z2 / (2 * Z2 + Zf) * (Vth_1 + (x[0] + 1j * x[1]) * Z2 - (x[2] + 1j * x[3]) * Z2)  # LL
     # V1 = (x[0] + 1j * x[1]) * Z1 + (Z2 + 3 * Zf) / (3 * Z2 + 6 * Zf) * ((x[0] + 1j * x[1]) * Z2 + (x[2] + 1j * x[3]) * Z2 + Vth_1)  # LLG
     
     return V1
 
 def V2(x):
     # V2 = 1 / (Zf + Z2) * ((x[2] + 1j * x[3]) * (Z2 * Zf + Z1 * Z2 + Z1 * Zf))  # balanced
-    # V2 = (x[2] + 1j * x[3]) * (Z1 + Z2) - Z2 / (3 * Zf + 3 * Z2) * ((x[2] + 1j * x[3]) * Z2 + (x[0] + 1j * x[1]) * Z2 + Vth_1)  # LG
-    V2 = Vth_1 + (x[0] + 1j * x[1]) * Z2 + (x[2] + 1j * x[3]) * Z1 - (Z2 + Zf) / (2 * Z2 + Zf) * (Vth_1 + (x[0] + 1j * x[1]) * Z2 - (x[2] + 1j * x[3]) * Z2)  # LL
+    V2 = (x[2] + 1j * x[3]) * (Z1 + Z2) - Z2 / (3 * Zf + 3 * Z2) * ((x[2] + 1j * x[3]) * Z2 + (x[0] + 1j * x[1]) * Z2 + Vth_1)  # LG
+    # V2 = Vth_1 + (x[0] + 1j * x[1]) * Z2 + (x[2] + 1j * x[3]) * Z1 - (Z2 + Zf) / (2 * Z2 + Zf) * (Vth_1 + (x[0] + 1j * x[1]) * Z2 - (x[2] + 1j * x[3]) * Z2)  # LL
     # V2 = (x[2] + 1j * x[3]) * Z1 + (Z2 + 3 * Zf) / (3 * Z2 + 6 * Zf) * ((x[0] + 1j * x[1]) * Z2 + (x[2] + 1j * x[3]) * Z2 + Vth_1)
 
     return V2
@@ -172,7 +172,7 @@ while (abs(V1_new - V1_old) > tol or abs(V2_new - V2_old) > tol) and compt < com
             Iabc_max = max(abs(Iabc_gc))
             x_gc = [np.real(I1_gc), np.imag(I1_gc), np.real(I2_gc), np.imag(I2_gc)]
             V012_gc = np.array([V0(x_gc), V1(x_gc), V2(x_gc)])
-            print(fac)
+            # print(fac)
 
 
     I012_gc = np.array([0, I1_gc, I2_gc])
@@ -182,12 +182,12 @@ while (abs(V1_new - V1_old) > tol or abs(V2_new - V2_old) > tol) and compt < com
     V012_gc = np.array([V0(x_gc), V1(x_gc), V2(x_gc)])
 
     V012f = V012_gc
-    print(abs(V012_gc))
-    # print(Iabc_max)
+    # print(abs(V012_gc))
+    print(Iabc_max)
 
     V1_new = abs(V012_gc[1])
     V2_new = abs(V012_gc[2])
 
-    print(compt)
+    # print(compt)
     compt += 1
 
