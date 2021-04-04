6d8c96ef4d7f96bbd06a902a485966681ec9cf7f
unknown <jfanals13@gmail.com>
2021-04-04 18:51:52 +0200

    2 converters optimization works fine

--- VSC faults/Code/n_converters/2_conv.py
+++ VSC faults/Code/n_converters/2_conv.py
@@ -1,61 +1,94 @@
 import numpy as np
-from Functions import xabc_to_012, x012_to_abc
+from scipy.optimize import minimize
+from Functions import xabc_to_012, x012_to_abc, build_static_objects 
 
-# Connections:
+# Functions
+def volt_solution(x):
+    m1_inv = static_objects[0]
+    m2 = static_objects[1]
+    Vg_v = static_objects[2] 
+
+    Ii_v = np.zeros((9,1), dtype=complex)
+    Ii_v[0:3] = [[x[0] + 1j * x[1]], [x[2] + 1j * x[3]], [x[4] + 1j * x[5]]]
+    Ii_v[3:6] = [[x[6] + 1j * x[7]], [x[8] + 1j * x[9]], [x[10] + 1j * x[11]]]
+
+    lhs = Ii_v - np.dot(m2, Vg_v)
+    Vv_v = np.dot(m1_inv, lhs)
+    return Vv_v
+
+def constraint_Imax(x):
+    I1a = abs(x[0] + 1j * x[1])
+    I1b = abs(x[2] + 1j * x[3])
+    I1c = abs(x[4] + 1j * x[5])
+    I2a = abs(x[6] + 1j * x[7])
+    I2b = abs(x[8] + 1j * x[9])
+    I2c = abs(x[10] + 1j * x[11])
+    return Imax - max(I1a, I1b, I1c, I2a, I2b, I2c)
+
+def constraint_Ire1(x):
+    return x[0] + x[2] + x[4] 
+
+def constraint_Iim1(x):
+    return x[1] + x[3] + x[5] 
+
+def constraint_Ire2(x):
+    return x[6] + x[8] + x[10] 
+
+def constraint_Iim2(x):
+    return x[7] + x[9] + x[11] 
+
+def objective_f(x):
+    Vv_v = volt_solution(x)
+    V_p1_abc = Vv_v[0:3]
+    V_p2_abc = Vv_v[3:6]
+    V_p1_012 = xabc_to_012(V_p1_abc)
+    V_p2_012 = xabc_to_012(V_p2_abc)
+
+    suma = lam_vec[0] * abs(1 - abs(V_p1_012[1])) + lam_vec[1] * abs(1 - abs(V_p2_012[1])) + lam_vec[2] * abs(0 - abs(V_p1_012[2])) + lam_vec[3] * abs(0 - abs(V_p2_012[2]))
+    return suma
+
+# Data
 V_mod = 1
+Imax = 1
 Zv1 = 0.01 + 0.05 * 1j
 Zv2 = 0.03 + 0.06 * 1j
 Zt = 0.01 + 0.1 * 1j
+Y_con = [0, 0, 0]  # Yab, Ybc, Yac
+Y_gnd = [10, 0, 0]  # Yag, Ybg, Yc
+lam_vec = [1, 1, 1, 1]  # V1p, V2p, V1n, V2n
+Ii_t = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-Yv1 = 1 / Zv1
-Yv2 = 1 / Zv2
-Yt = 1 / Zt
+static_objects = build_static_objects(V_mod, Zv1, Zv2, Zt, Y_con, Y_gnd)
 
-# Faults
-Yab = 0
-Ybc = 0
-Yac = 0
+# Optimize
+con1 = {'type': 'ineq', 'fun': constraint_Imax}
+con2 = {'type': 'eq', 'fun': constraint_Ire1}
+con3 = {'type': 'eq', 'fun': constraint_Iim1}
+con4 = {'type': 'eq', 'fun': constraint_Ire2}
+con5 = {'type': 'eq', 'fun': constraint_Iim2}
+cons = [con1, con2, con3, con4, con5]
 
-Zag = 0.2
-Yag = 1 / Zag
-Ybg = 0
-Ycg = 0
+sol = minimize(objective_f, Ii_t, method='SLSQP', constraints=cons, options={'ftol':1e-9})
+I_sol = sol.x
 
-# Admittance matrices
-m0 = np.zeros((3,3), dtype=complex)
+# Manage results
+I1_abc = [I_sol[0] + 1j * I_sol[1], I_sol[2] + 1j * I_sol[3], I_sol[4] + 1j * I_sol[5]]
+I2_abc = [I_sol[6] + 1j * I_sol[7], I_sol[8] + 1j * I_sol[9], I_sol[10] + 1j * I_sol[11]]
+V_f = volt_solution(I_sol)
+V_p1_012 = xabc_to_012(V_f[0:3])
+V_p2_012 = xabc_to_012(V_f[3:6])
+Ip1_012 = xabc_to_012(I1_abc)
+Ip2_012 = xabc_to_012(I2_abc)
+Ip1_1 = Ip1_012[1] * np.exp(-1j * np.angle(V_p1_012[1]))
+Ip1_2 = Ip1_012[2] * np.exp(-1j * np.angle(V_p1_012[2]))
+Ip2_1 = Ip2_012[1] * np.exp(-1j * np.angle(V_p2_012[1]))
+Ip2_2 = Ip2_012[2] * np.exp(-1j * np.angle(V_p2_012[2]))
 
-Yv1_m = np.zeros((3,3), dtype=complex)
-np.fill_diagonal(Yv1_m, Yv1)
 
-Yv2_m = np.zeros((3,3), dtype=complex)
-np.fill_diagonal(Yv2_m, Yv2)
-
-Yt_m = np.zeros((3,3), dtype=complex)
-np.fill_diagonal(Yt_m, Yt)
-
-Yf_m = Yv1_m + Yv2_m + Yt_m
-Yf_m[0,:] += [Yag + Yab + Yac, -Yab, -Yac]
-Yf_m[1,:] += [-Yab, Ybg + Yab + Ybc, -Ybc]
-Yf_m[2,:] += [-Yac, -Ybc, Ycg + Yac + Ybc]
-
-m1 = np.block([[Yv1_m, m0, -Yv1_m], [m0, Yv2_m, -Yv2_m], [-Yv1_m, -Yv2_m, Yf_m]])
-m2 = np.block([[m0, m0, m0], [m0, m0, m0], [m0, m0, -Yt_m]])
-
-Vg_v = np.zeros((9,1), dtype=complex)
-a = 1 * np.exp(-120 * np.pi / 180 * 1j)
-Vg_v[6:9] = [[V_mod], [V_mod * a ** 2], [V_mod * a]]
-
-Ii_v = np.zeros((9,1), dtype=complex)
-Ii_v[0:3] = [[0.5], [-0.3 + 0.2 * 1j], [-0.2 - 0.2 * 1j]]
-Ii_v[3:6] = [[0.5], [-0.3 + 0.2 * 1j], [-0.2 - 0.2 * 1j]]
-
-lhs = Ii_v - np.dot(m2, Vg_v)
-mat_v = np.dot(np.linalg.inv(m1), lhs)
-
-V_p1 = mat_v[0:3]
-V_p2 = mat_v[3:6]
-V_f = mat_v[6:9]
-
-print(V_p1)
-print(V_p2)
-print(V_f)
+# Print results
+print('PCC1 +- absolute voltages: ', abs(V_p1_012[1]), abs(V_p1_012[2]))
+print('PCC2 +- absolute voltages: ', abs(V_p2_012[1]), abs(V_p2_012[2]))
+print(Ip1_1)
+print(Ip1_2)
+print(Ip2_1)
+print(Ip2_2)
--- VSC faults/Code/n_converters/Functions.py
+++ VSC faults/Code/n_converters/Functions.py
@@ -35,3 +35,66 @@ def x012_to_abc(V012):
 
     Vabc = np.dot(T, V012)
     return Vabc
+
+
+def build_static_objects(V_mod, Zv1, Zv2, Zt, Y_con, Y_gnd):
+    # Admittances to build the matrices
+    Yv1 = 1 / Zv1
+    Yv2 = 1 / Zv2
+    Yt = 1 / Zt
+
+    # Admittance matrices
+    m0 = np.zeros((3,3), dtype=complex)
+
+    Yv1_m = np.zeros((3,3), dtype=complex)
+    np.fill_diagonal(Yv1_m, Yv1)
+
+    Yv2_m = np.zeros((3,3), dtype=complex)
+    np.fill_diagonal(Yv2_m, Yv2)
+
+    Yt_m = np.zeros((3,3), dtype=complex)
+    np.fill_diagonal(Yt_m, Yt)
+
+    Yf_m = Yv1_m + Yv2_m + Yt_m
+    Yf_m[0,:] += [Y_gnd[0] + Y_con[0] + Y_con[2], -Y_con[0], -Y_con[2]]
+    Yf_m[1,:] += [-Y_con[0], Y_gnd[1] + Y_con[0] + Y_con[1], -Y_con[1]]
+    Yf_m[2,:] += [-Y_con[2], -Y_con[1], Y_gnd[2] + Y_con[2] + Y_con[1]]
+
+    m1 = np.block([[Yv1_m, m0, -Yv1_m], [m0, Yv2_m, -Yv2_m], [-Yv1_m, -Yv2_m, Yf_m]])
+    m2 = np.block([[m0, m0, m0], [m0, m0, m0], [m0, m0, -Yt_m]])
+    m1_inv = np.linalg.inv(m1)
+
+    Vg_v = np.zeros((9,1), dtype=complex)
+    a = np.exp(120 * np.pi / 180 * 1j)
+    Vg_v[6:9] = [[V_mod], [V_mod * a ** 2], [V_mod * a]]
+
+    return [m1_inv, m2, Vg_v]
+
+
+def volt_solution(x):
+    m1_inv = static_objects[0]
+    m2 = static_objects[1]
+    Vg_v = static_objects[2] 
+
+    Ii_v = np.zeros((9,1), dtype=complex)
+    Ii_v[0:3] = [[x[0]], [x[1]], [x[2]]]
+    Ii_v[3:6] = [[x[3]], [x[4]], [x[5]]]
+
+    lhs = Ii_v - np.dot(m2, Vg_v)
+    Vv_v = np.dot(m1_inv, lhs)
+    return Vv_v
+
+
+def constraint_Imax(x):
+    return Imax - max(abs(x))
+
+
+def objective_f(x):
+    suma = 0
+    Vv_v = volt_solution(static_objects, x)
+    for kk in range(int(len(V12_vec) / 2)):
+        suma += lam_vec[kk] * (abs(1 - abs(V12_vec[kk]))) 
+    for kk in range(int(len(V12_vec) / 2), len(V12_vec)):
+        suma += lam_vec[kk] * (abs(0 - abs(V12_vec[kk])))
+    return suma
+
