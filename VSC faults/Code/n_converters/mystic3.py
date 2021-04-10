f4fa72785e1f6a9d88ead1fa437ec6dc2bb4a1ce
unknown <jfanals13@gmail.com>
2021-04-10 22:48:35 +0200

    mystic for 3 variables works fine

--- VSC faults/Code/n_converters/mystic3.py
+++ VSC faults/Code/n_converters/mystic3.py
@@ -1,19 +1,23 @@
 def objective(x):
     return 1
 
-equations = """
-x0 + x1 == 0
+equations_c = """
+x0 + x1 + x2 == 0.3
 """
 
-bounds = [(-1,1),(-1,1)] #unnecessary
+def obj_fun(x):
+    return x[0] ** 2 + x[1] - x[2] + 0.56
+
+bounds = [(-1,1),(-1,1),(-1,1)] #unnecessary
+xx0 = [0,0,0]
 
 from mystic.symbolic import generate_constraint, generate_solvers, simplify
 from mystic.symbolic import generate_penalty, generate_conditions    
-from mystic.solvers import diffev2
+from mystic.solvers import diffev2, fmin_powell, fmin
 
-cf = generate_constraint(generate_solvers(simplify(equations)))
+cf = generate_constraint(generate_solvers(simplify(equations_c)))
 
-result = diffev2(objective, x0=bounds, bounds=bounds, \
+result = fmin(obj_fun, x0=xx0, bounds=bounds, \
                  constraints=cf, \
                  npop=40, gtol=50, disp=False, full_output=True)
 
