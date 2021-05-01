d45c8a5afacb7427ab112ffdafa7edcd5c26b97c
unknown <jfanals13@gmail.com>
2021-05-02 00:11:42 +0200

    RX ratio 3x

--- VSC faults/Code/n_converters/Main_2VSC.py
+++ VSC faults/Code/n_converters/Main_2VSC.py
@@ -13,18 +13,18 @@ Imax = 1
 Zv1 = 0.01 + 0.05 * 1j
 Zv2 = 0.02 + 0.06 * 1j
 Zt = 0.01 + 0.1 * 1j
-Y_con = [0, 0, 0]  # Yab, Ybc, Yac
+Y_con = [10, 10, 10]  # Yab, Ybc, Yac
 Y_gnd = [0, 0, 0]  # Yag, Ybg, Yc
 lam_vec = [1, 1, 1, 1]  # V1p, V2p, V1n, V2n
 Ii_t = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  # currents initialization: Ia1re, Ia1im, ...
 Ii_t0 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  # currents initialization: Ia1re, Ia1im, ...
-type_f = 'opt_LLG_'
-folder = 'Results_2conv_Zf_v1/'
+type_f = 'opt_3x_'
+folder = 'Results_2conv_RX_v1/'
 
 # RX variation
-n_p = 50
-# [RX_vec, Zin_vec] = fZ_rx(5, 0.1, n_p, abs(Zv1))  # lim1, lim2, n_p, Zthmod
-Yf_vec = fY_fault(15, 200, n_p)  # for values big enough to have a severe fault
+n_p = 100
+[RX_vec, Zin_vec] = fZ_rx(2, 0.01, n_p, abs(Zv1))  # lim1, lim2, n_p, Zthmod
+# Yf_vec = fY_fault(15, 200, n_p)  # for values big enough to have a severe fault
 # lam1_vec = f_lam(1.0, 0.0, n_p)
 
 # Store data
@@ -51,9 +51,9 @@ for iik in range(n_p):
     # Y_gnd = [Yf_vec[iik], Yf_vec[iik], Yf_vec[iik]]
     # Y_gnd = [Yf_vec[iik], 0, 0]
     # Y_con = [Yf_vec[iik], 0, 0]
-    Y_con = [1000, 0, 0]
-    Y_gnd = [Yf_vec[iik], 0, 0]
-    # Zv1 = Zin_vec[iik]
+    # Y_con = [1000, 0, 0]
+    # Y_gnd = [Yf_vec[iik], 0, 0]
+    Zv1 = Zin_vec[iik]
     # lam_vec = [lam1_vec[iik], 1 - lam1_vec[iik], 0, 0]
 
     if iik > 0:
@@ -108,8 +108,8 @@ for iik in range(n_p):
 
 
 # Save csv
-x_vec = Yf_vec
-# x_vec = RX_vec
+# x_vec = Yf_vec
+x_vec = RX_vec
 # x_vec = lam1_vec
 pcnt = 1
 n_pp = int((1-pcnt) * n_p)
