c1b3d62e774f22dc5e55d975a607ef4e1250cf4d
unknown <jfanals13@gmail.com>
2021-05-10 00:43:27 +0200

    results with Zf, not Yf

--- VSC faults/Code/n_converters/Main_2VSC_new.py
+++ VSC faults/Code/n_converters/Main_2VSC_new.py
@@ -13,8 +13,8 @@ Imax = 1
 Zv1 = 0.01 + 0.05 * 1j
 Zv2 = 0.02 + 0.06 * 1j
 Zt = 0.01 + 0.1 * 1j
-Y_con = [10000, 0, 0]  # Yab, Ybc, Yac
-Y_gnd = [25, 0, 0]  # Yag, Ybg, Yc
+Y_con = [0, 0, 0]  # Yab, Ybc, Yac
+Y_gnd = [0, 0, 0]  # Yag, Ybg, Yc
 lam_vec = [1, 1, 1, 1]  # V1p, V2p, V1n, V2n
 Ii_t = [0.9942, 0.1075, -0.9941, -0.109, -0.000132, 0.00149, 0.6149, -0.7886, -0.6195, 0.785, 0.0046, 0.00361]  # currents initialization: Ia1re, Ia1im, ...
 # Ii_t = [0.22, -0.97, 0.01, 1, -0.23, -0.03, 0.28, -0.96, -0.02, 1, -0.26, -0.04]  # currents initialization: Ia1re, Ia1im, ...
@@ -53,7 +53,7 @@ for iik in range(n_p):
     # Y_con = [Yf_vec[iik], 0, 0]
     # Y_con = [1000, 0, 0]
     # Y_gnd = [Yf_vec[iik], 0, 0]
-    Zv1 = Zin_vec[iik]
+    # Zv1 = Zin_vec[iik]
     # lam_vec = [lam1_vec[iik], 1 - lam1_vec[iik], 0, 0]
 
 
@@ -105,8 +105,8 @@ for iik in range(n_p):
 
 
 # Save csv
-# x_vec = Yf_vec
-x_vec = RX_vec
+x_vec = Yf_vec
+# x_vec = RX_vec
 # x_vec = lam1_vec
 pcnt = 1
 n_pp = int((1-pcnt) * n_p)
