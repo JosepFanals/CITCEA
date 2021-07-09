6f350a3db6553258b1e0362eb39dde916fdba0a9
unknown <jfanals13@gmail.com>
2021-07-09 22:46:24 +0200

    Imod

---
+++ VSC faults/Publication/Data/Calc_Imod/Main.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+
+folder = 'LL_Zf_1conv/'
+file1 = 'opt_LL_In1re.csv'
+file2 = 'opt_LL_In1im.csv'
+
+df1 = pd.read_csv(folder+file1)
+df2 = pd.read_csv(folder+file2)
+df3 = df1.copy()
+df3['y2'] = df2['y'].to_numpy()
+df3['mod'] = ""
+for ll in range(len(df3)):
+	df3.iloc[ll, 3] = np.sqrt(df3.iloc[ll, 1] ** 2 + df3.iloc[ll, 2] ** 2)
+df4 = df3.drop(columns=['y','y2'])
+df5 = df4.rename(columns={"mod": "y"})
+
+file_result = 'res_opt_LL_In.csv'
+print(df5)
+# print(folder_result)
+df5.to_csv(folder+file_result)
